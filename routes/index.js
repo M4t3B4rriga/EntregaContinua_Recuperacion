04a225c6b6421988d798ec2e7817a5e407b94c21
M4t3B4rriga <msbarriga@espe.edu.ec>
2025-06-30 12:31:17 -0500

    pagina web

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+// filepath: login-app/routes/index.js
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/login');
 });
 
+// Export the router
 module.exports = router;
